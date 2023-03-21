8822b9d04e614c13b8d1fb49fb1d9e989e44a624
yosef-w <ywolday24@gmail.com>
2023-03-21 13:48:21 -0500

    Create fake data folder

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
+from fake_data import posts
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/test')
-def hello_test():
-    return 'This is a test!'
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
