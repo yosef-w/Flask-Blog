3314f20c37ebfa2280b9d64d0ba27978e5ba30d1
yosef-w <ywolday24@gmail.com>
2023-03-21 14:11:55 -0500

    Add login intro if logged in and make searchbar sticky

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,14 @@ from flask import render_template
 from fake_data import posts
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', posts=posts, logged_in=False)
+
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @app.route('/login')
 def login():
