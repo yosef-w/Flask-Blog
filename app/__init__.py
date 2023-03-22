6dcbd4360abac1a951a3f25820d10e7b4418b6de
yosef-w <ywolday24@gmail.com>
2023-03-22 15:13:37 -0500

    add migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes file into the current package
-from app import routes
+from app import routes, models
 # or from . import routes
