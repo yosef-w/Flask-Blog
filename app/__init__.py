e3766f8beb7d8ae26026178e2f43b6f0f0e1a696
yosef-w <ywolday24@gmail.com>
2023-03-22 14:27:04 -0500

    Add config and env

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 # import all of the routes from the routes file into the current package
